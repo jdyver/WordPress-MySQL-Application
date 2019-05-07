f792e2679dc8a45a0281ec27f506abb2ae6fedae
James Dyckowski <spam@nowhere.com>
2019-05-07 10:19:17 -0400

    JD-GIT AUTO: Finished shell creation

--- README.md
+++ README.md
@@ -17,3 +17,18 @@ Kubectl is setup to k8s cluster
 $ bash WordPressExample.sh up
 OUTPUT
 ```
+
+### WordPressExample.sh down
+```
+$ bash WordPressExample.sh down
+WordPressExample.sh - Down
+
+secret "mysql-pass-fc4g29gt4c" deleted
+service "wordpress-mysql" deleted
+service "wordpress" deleted
+deployment.apps "wordpress-mysql" deleted
+deployment.apps "wordpress" deleted
+persistentvolumeclaim "mysql-pv-claim" deleted
+persistentvolumeclaim "wp-pv-claim" deleted
+WordPressExample.sh - Application Removed
+```
--- WordPressExample.sh
+++ WordPressExample.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# WordPressExample.sh up | down
-# - Up offline is default (using local files)
-# - Down removes the app
+# WordPressExample.sh -u | up | -d | down
+# - Up: Deploy app
+# - Down Remove app
 
 # Requires: Kubectl setup to k8s cluster
 
 # TODO - Check kubectl get nodes
 
 case "$1" in
-up)
+-u | [uU][pP])
 	echo "WordPressExample.sh - Online"
 	echo
-#    kubectl apply -k ./Files/
+    kubectl apply -k ./Files/
             # $ kubectl apply -k ./Files/
             # secret/mysql-pass-fc4g29gt4c created
             # service/wordpress-mysql created
@@ -34,11 +34,10 @@ up)
         then
             printf "."
         else
-            echo
             echo "WordPressExample.sh - WordPress Secret Created"
-            echo "$ kubectl get secrets"
+            echo " $ kubectl get secrets"
             TASK=$(kubectl get secrets)
-            echo "$TASK"
+            echo " $TASK"
             echo
             break
         fi
@@ -53,44 +52,105 @@ up)
             # NAME             STATUS   VOLUME                                     CAPACITY   ACCESS MODES   STORAGECLASS     AGE
             # mysql-pv-claim   Bound    pvc-7cb74f0a-702a-11e9-a26b-0a4489458944   20Gi       RWO            ebs-csi-driver   4m54s
             # wp-pv-claim      Bound    pvc-7cd30722-702a-11e9-a26b-0a4489458944   20Gi       RWO            ebs-csi-driver   4m54s
-        TASK=$(kubectl get pvc | grep mysql | awk '{print $2}' | grep Bound)
+        TASK=$(kubectl get pvc | grep mysql-pv | awk '{print $2}' | grep Bound)
         if [ "$TASK" != "Bound" ]
         then
             printf "."
         else
-            echo
-            echo "WordPressExample.sh - WordPress MySQL Storage Bind Created"
-            echo "$ kubectl get pvc"
+            echo "WordPressExample.sh - MySQL Storage Bind Created"
+            echo " $ kubectl get pvc"
             TASK=$(kubectl get pvc | grep NAME)
-            echo "$TASK"
+            echo " $TASK"
+            TASK=$(kubectl get pvc | grep mysql-pv)
+            echo " $TASK"
             echo
             break
         fi
     	sleep 10
-    done        
+    done
+    while true
+	do
+            # $ kubectl get pvc
+            # NAME             STATUS   VOLUME                                     CAPACITY   ACCESS MODES   STORAGECLASS     AGE
+            # mysql-pv-claim   Bound    pvc-7cb74f0a-702a-11e9-a26b-0a4489458944   20Gi       RWO            ebs-csi-driver   4m54s
+            # wp-pv-claim      Bound    pvc-7cd30722-702a-11e9-a26b-0a4489458944   20Gi       RWO            ebs-csi-driver   4m54s
+        TASK=$(kubectl get pvc | grep wp-pv | awk '{print $2}' | grep Bound)
+        if [ "$TASK" != "Bound" ]
+        then
+            printf "."
+        else
+            echo "WordPressExample.sh - WordPress Storage Bind Created"
+            echo " $ kubectl get pvc"
+            TASK=$(kubectl get pvc | grep NAME)
+            echo " $TASK"
+            TASK=$(kubectl get pvc | grep wp-pv)
+            echo " $TASK"
+            echo
+            break
+        fi
+    	sleep 10
+    done
+	NAME1=$(kubectl get pods | grep wordpress | awk '{print $1}' | sed -n 1p)
+	NAME2=$(kubectl get pods | grep wordpress | awk '{print $1}' | sed -n 2p)
             # $ kubectl get pods
             # NAME                               READY   STATUS    RESTARTS   AGE
             # wordpress-599d5b78f6-7kksv         1/1     Running   0          5m4s
             # wordpress-mysql-767cb86586-hclv8   1/1     Running   0          5m4s
+
+	## Check wordpress NAME1
+	while true
+	do
+    	# Get pod status
+    	TASK=$(kubectl get pods | grep $NAME1 | awk '{print $3}')
+
+      if [ "$TASK" != "Running" ]
+      then
+           	printf "."
+      else
+            echo
+            echo "WordPressExample.sh - WordPress Deployment Up"
+            break
+      fi
+    	sleep 5
+	done
+
+	## Check wordpress-sql NAME2
+	while true
+	do
+    	# Get pod status
+    	TASK=$(kubectl get pods | grep $NAME2 | awk '{print $3}')
+
+      if [ "$TASK" != "Running" ]
+      then
+           	printf "."
+      else
+            echo
+            echo "WordPressExample.sh - MySQL Deployment Up"
+            break
+      fi
+    	sleep 5
+	done
+    NAME1=$(kubectl get services | grep wordpress | sed -n 1p | awk '{print $4}')
             # $ kubectl get services
             # NAME              TYPE           CLUSTER-IP    EXTERNAL-IP                                                               PORT(S)        AGE
             # kubernetes        ClusterIP      10.0.0.1      <none>                                                                    443/TCP        136m
             # wordpress         LoadBalancer   10.0.41.182   a7c5e8cc1702a11e9a26b0a448945894-1706311152.us-west-2.elb.amazonaws.com   80:30203/TCP   9m57s
             # wordpress-mysql   ClusterIP      None          <none>                                                                    3306/TCP       9m57s
-    echo "WordPress is up and accessible at: <External-IP Above>
+    echo "WordPress is up and accessible at: http://$NAME1
     ** Login and setup username and password or remove application immediately **"
     echo
 	;;
-down)
+-d | [dD][oO][wW][nN])
 	echo "WordPressExample.sh - Down"
 	echo
     kubectl delete -k ./Files/
+    echo
 	echo "WordPressExample.sh - Application Removed"
 	echo
 	;;
 *)
-	echo "Fail - WordPressExample.sh up | down 
-	- Error in Argument 1"
+	echo "Fail - WordPressExample.sh -u | up | -d | down"
+    echo " - Error in Argument 1"
 	echo
 	exit
 	;;
